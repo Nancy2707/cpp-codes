8bd861f552330f76366bff26dc73e8bf527eff40
Nancy Sharma <nancysharma2000a@gmail.com>
2024-07-17 20:13:43 +0530

    Create code.cpp

---
+++ DSA Basics/Finding the Pivot Element in an Array/code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findPivot(vector<int>& arr) {
+    int low = 0;
+    int high = arr.size() - 1;
+    
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        
+  if(arr[mid]>arr[mid+1]){
+      return arr[mid+1];
+  }
+  else if(arr[mid]>=arr[mid-1]){
+      low = mid + 1;
+  }
+  else{
+      high = mid;
+  }
+}
+}
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 8, 1, 2};
+    
+    int pivot = findPivot(arr);
+    cout << "Pivot element: " << pivot << endl;
+    
+    return 0;
+}
