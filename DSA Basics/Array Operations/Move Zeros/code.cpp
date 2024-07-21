aa7979ed1edc482afd0b2b624e8c5229b0b2c1fe
Nancy Sharma <nancysharma2000a@gmail.com>
2024-07-21 19:39:35 +0530

    Create code.cpp

---
+++ DSA Basics/Array Operations/Move Zeros/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int insertPos = 0; 
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != 0) {
+                nums[insertPos++] = nums[i];
+            }
+        }
+        while (insertPos < n) {
+            nums[insertPos++] = 0;
+        }
+    }
+};
